26a8df682ab781fba049fe0f8a725437ffbcef27
Miłosz Skrzypiec <skrzypiec.business@gmail.com>
2023-01-15 10:04:22 +0100

    fixed piece sprites

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+
 set -xe
+
 g++ -c -o pograph/pograph.o pograph/pograph.cpp
 ar rcs pograph/libpograph.a pograph/pograph.o
+mkdir include
 cp pograph/pograph.h include/
+mkdir lib
 cp pograph/libpograph.a lib/
 g++ -Iinclude/ -o fentastic fentastic.cpp lib/libpograph.a
